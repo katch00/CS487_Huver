38da6d7210b28e9ef6ef17e799e18ef91689bb86
katch00 <nstroupe0215@gmail.com>
2021-04-16 00:35:09 +0000

    add driver and customer class

---
+++ src/main/java/myapp/Customer.java
@@ -0,0 +1,34 @@
+package main.java.myapp;
+
+public class Customer {
+
+    String firstName;
+    String lastName;
+    String username;
+    int birthyear;
+    int birthmonth;
+    int birthday;
+    String number;
+    String address;
+    String password;
+    int cardNo;
+    
+    String type = "customer";
+
+    public Customer(String firstName, String lastName, String username, int birthday, int birthmonth, int birthyear, String number,
+           String address, String password, int cardNo)
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.birthyear = birthyear;
+        this.birthday = birthday;
+        this.birthmonth = birthmonth;
+        this.number = number;
+        this.address = address;
+        this.password = password;
+        this.cardNo = cardNo;
+
+    }
+
+}
