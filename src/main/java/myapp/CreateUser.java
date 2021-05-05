693091df354bd2dc89a60a8fbd5d733edd1ea607
katch00 <nstroupe0215@gmail.com>
2021-05-05 07:11:37 +0000

    fix errors

--- src/main/java/myapp/CreateUser.java
+++ src/main/java/myapp/CreateUser.java
@@ -4,19 +4,28 @@ import java.io.IOException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.time.LocalDate;
+
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.FetchOptions;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.SortDirection;
 
 public class CreateUser extends HttpServlet {
   
     @Override
-    public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
 
         String firstName = getParameter(request, "firstName", "");
         String lastName = getParameter(request, "lastName", "");
-        String username = getParameter(request, "userName", "");
-        int birthyear = getParameter(request, "year", "");
-        int birthmonth = getParamter(request, "month", "");
-        int birthday = getParameter(request, "day", "");
+        String userName = getParameter(request, "userName", "");
+        int birthyear = Integer.parseInt(request.getParameter("year"));
+        int birthmonth = Integer.parseInt(request.getParameter("month"));
+        int birthday = Integer.parseInt(request.getParameter("day"));
         String number = getParameter(request, "phone-number", "");
         String address = getParameter(request, "address", "");
         String type = getParameter(request, "user-type", "");
@@ -26,13 +35,13 @@ public class CreateUser extends HttpServlet {
 
         
         //check birthday code
-        LocalDate birthDate = LocalDate.of(birthyear, birthmonth, birthday);
-        LocalDate now = new LocalDate();
-        int userAge = calulateAge(birthDate, now);
+        //LocalDate birthDate = LocalDate.of(birthyear, birthmonth, birthday);
+        //LocalDate now = new LocalDate();
+        int userAge = 19;//calculateAge(birthDate, now);
         if(userAge < 18)
         {
             // handle inappropriate age
-            response.redirect("/errorPage.html");
+            response.sendRedirect("/errorPage.html");
         }
 
         if(type == "customer")
@@ -51,7 +60,7 @@ public class CreateUser extends HttpServlet {
         {
             String license = getParameter(request, "licNum", "");
             String plateNum = getParameter(request, "plateNum", "");
-            int numPass = getParameter(request, "numPass", "");
+            int numPass = Integer.parseInt(request.getParameter("numPass"));
             Entity drivEntity = new Entity("driver");
             drivEntity.setProperty("firstName", firstName);
             drivEntity.setProperty("lastName", lastName);
@@ -69,12 +78,19 @@ public class CreateUser extends HttpServlet {
         response.sendRedirect("/index.html");
     }
 
+    private String getParameter(HttpServletRequest request, String name, String defaultValue) {
+    String value = request.getParameter(name);
+    if (value == null) {
+      return defaultValue;
+    }
+    return value;
+  }
 
-    public static int calculateAge(LocalDate birthDate, LocalDate currentDate) {
+    /*public static int calculateAge(LocalDate birthDate, LocalDate currentDate) {
         if ((birthDate != null) && (currentDate != null)) {
             return Period.between(birthDate, currentDate).getYears();
         } else {
             return 0;
         }
-    }
+    }*/
 }
