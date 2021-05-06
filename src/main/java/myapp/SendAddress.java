b524f3a2711e429c6e61613cf171df51e6a80f98
katch00 <nstroupe0215@gmail.com>
2021-05-06 01:05:42 +0000

    store favorites if logged in

--- src/main/java/myapp/SendAddress.java
+++ src/main/java/myapp/SendAddress.java
@@ -5,12 +5,47 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+
+import javax.servlet.http.*;
+
 public class SendAddress extends HttpServlet {
     
     @Override
     public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        String staddr = getParameter(req, "staddr", "");
         String city = getParameter(req, "city", "");
+        String zip = getParameter(req, "zip", "");
         String numPass = getParameter(req, "numPass", "");
+        String isFavorite = getParameter(req, "isFav", "no");
+
+        if(isFavorite.equals("yes"))
+        {
+            DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+            Entity favEntity = new Entity("Favorites");
+            String username = null;
+            try{
+                HttpSession session=req.getSession(false);
+                
+                username = (String) session.getAttribute("username");
+                System.out.println("Name: " + username);
+            }catch(Exception exp){
+                System.out.println(exp);
+                resp.sendRedirect("/index.html");
+            }
+
+            if(username != null)
+            {
+                favEntity.setProperty("user", username);
+                favEntity.setProperty("staddr", staddr);
+                favEntity.setProperty("city", city);
+                favEntity.setProperty("zip", zip);
+                datastore.put(favEntity);
+            }
+
+        }
         
         resp.sendRedirect("/showDrivers.html?city=" + city + "&numPass=" + numPass);
     }
