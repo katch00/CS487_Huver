3bedffeb54387b64767557ce20c61210a56d3d8e
katch00 <nstroupe0215@gmail.com>
2021-05-06 20:43:33 +0000

    send email to user on purchase

---
+++ src/main/java/myapp/Key.java
@@ -0,0 +1,5 @@
+package main.java.myapp;
+public class Key {
+    public static final String API_KEY = "6b2fbe12d13fc1c476c2862971b17e9d";
+    public static final String API_SECRET = "5806b0e58f4ce7ad291170e5f8ac4995";
+}
